44a36ef1cdc78752828a585a1c5b2418d63e1a72
JohnTseng0918 <s9141302@gmail.com>
2020-11-17 17:18:47 +0800

    add OMP algorithm

---
+++ Program_HW2/omp.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def OMP(sparsity, x, B):
+    row, col = B.shape
+    not_selected_indices = list(range(row))
+    selected_indices = []
+
+    r = np.copy(x)
+
+    for i in range(sparsity):
+        idx = np.argmax(B[not_selected_indices] @ r.T)
+        selected_indices.append(not_selected_indices[idx])
+        not_selected_indices.remove(not_selected_indices[idx])
+        selected_indices.sort()
+        c = np.linalg.pinv(B[selected_indices] @ B[selected_indices].T) @ B[selected_indices] @ x
+        r = x - B[selected_indices].T @ c
+    return B[selected_indices].T @ c, sum(r**2)
